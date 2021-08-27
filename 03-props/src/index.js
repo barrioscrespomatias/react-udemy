ab0f64de4c21b6f94f029fffd99fe01078548852
matisfd <matisfd@gmail.com>
2021-08-27 11:40:57 -0300

    init counter app

--- 03-props/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import PrimeraApp from './PrimeraApp';
-
-// const saludo = <h1>Hola mundos!</h1>;
-
-const divRoot = document.querySelector('#root');
-
-ReactDom.render(<PrimeraApp></PrimeraApp>,divRoot);
