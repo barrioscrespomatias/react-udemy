8f788c889c65353c1f2bf817f770282a72bff160
matisfd <matisfd@gmail.com>
2021-08-28 16:04:15 -0300

    Se agregan comentarios al codigo

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -6,9 +6,14 @@ import PropTypes from 'prop-types'
 const GifExpertApp = props => {
 
     const [category, setCategory] = useState(['Dragon ball','Caballeros del zodíaco','Tortugas ninjas']);
-
+    
+    /**
+     * Set category pisa el estado actual del array donde se encuentran las categorias [category]. 
+     * No se puede pisar directamente con un string ya que sino el objeto deja de ser un array y pasa a ser un strting
+     * 
+     */
     const handleAdd = ()=>{
-        setCategory( (beforeCategory)=>[...category, 'Power Rangers']);
+        setCategory( (cats)=>[...cats, 'Power Rangers']);
     }
 
     console.log(category);
@@ -17,7 +22,13 @@ const GifExpertApp = props => {
     <Fragment>
         <h2>GifExpertApp</h2>
         <hr></hr>
-
+       
+        {/* 
+        Se utiliza cono id el item actual de la categoría. Esto tira un error si quisiera
+        agregar nuevamente el mismo elemento al array ya que no se podrían definir dos 
+        elementos con el mismo ID.
+        Se soluciona tomando el ID de la base de datos, o de la API */}
+        
         <ol>
             {
                 category.map((item)=>{
